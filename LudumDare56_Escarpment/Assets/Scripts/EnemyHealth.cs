3763a7046cc2cf74431d7c59a8743641c6be739f
LHKadosa <38136809+LHKadosa@users.noreply.github.com>
2024-10-07 22:17:41 +0200

    Some level design push

--- LudumDare56_Escarpment/Assets/Scripts/EnemyHealth.cs
+++ LudumDare56_Escarpment/Assets/Scripts/EnemyHealth.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    public int maxHealth;
     int hitCount = 0;
     private void OnParticleCollision(GameObject other)
     {
         hitCount++;
         Debug.Log("Hits taken" + hitCount);
-        if (hitCount >= 5)
+        if (hitCount >= maxHealth)
         {
             Destroy(this.gameObject);
         }
